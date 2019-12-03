b030a3792bc3e448c93a5e1be58989b575cc3170
Vegam-Vijesh <vijesh@aureoleinc.com>
2019-12-03 15:27:28 +0530

    My test

---
+++ BussinessSolution/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BussinessSolution
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            LoginWindow loginWindow = new LoginWindow();
+            MainWindow mainWindow = new MainWindow();
+
+            loginWindow.ShowDialog();
+            if (!loginWindow.DialogResult.HasValue || !loginWindow.DialogResult.Value)
+            {
+                Application.Current.Shutdown();
+            }
+
+            mainWindow.Show();
+        }
+    }
+
+
+}
